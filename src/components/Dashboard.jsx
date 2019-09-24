5b5cd0d70c974843a58706813212e5fd9b8e9c86
Matt M <matt.d.m@hotmail.com>
2019-09-24 10:35:54 -0500

    added dummy data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import {Route} from 'react-router-dom';
+import { connect } from 'react-redux';
 import Search from './Search';
 
-const Dashboard = ({match}) =>{
+const Dashboard = ({match, search}) =>{
 
     const [currentSearch, setCurrentSearch] = useState();
-
+    console.log(search);
     const testEvent = searchField =>{
         console.log(`Searching for information in search bar.. ${searchField.value}`);
         setCurrentSearch(searchField.value);
@@ -17,6 +18,7 @@ const Dashboard = ({match}) =>{
             <Search type="music" placeholder="Search for a song" changeEvent={testEvent} eventTimer="800"/>
             {currentSearch && <div className="searchResultTitle"><h2>Search results for {currentSearch}:</h2></div>}
 
+            { search.songList.map(song => <p>{song.track_name}</p>)}
             
 
             {/* <Route path={`${match.url}/profile`} render={ ()=>{
@@ -32,4 +34,4 @@ const Dashboard = ({match}) =>{
     );
 };
 
-export default Dashboard;
+export default connect(({search}) => ({search}), {})(Dashboard);
--- src/store/reducers/searchReducer.js
+++ src/store/reducers/searchReducer.js
@@ -1,4 +1,7 @@
-const initialState = {};
+import { dummyData } from '../local_data'
+const initialState = {
+    songList : dummyData,
+};
 
 export const searchReducer = (state = initialState, action) => {
     switch( action.type ) {
