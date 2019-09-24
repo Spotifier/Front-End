85971dcc14db9f14634dcbbba4ae563a9df7d166
Matt Masters <Matt.d.m@hotmail.com>
2019-09-24 14:21:43 -0500

    Revert "Dev"

--- src/store/reducers/dashaboardReducer.js
+++
@@ -1,8 +0,0 @@
-const initialState = {};
-
-export const dashboardReducer = (state = initialState, action) => {
-    switch( action.type ) {
-        default:
-            return state;
-    }
-}
--- src/store/reducers/searchReducer.js
+++
@@ -1,8 +0,0 @@
-const initialState = {};
-
-export const searchReducer = (state = initialState, action) => {
-    switch( action.type ) {
-        default:
-            return state;
-    }
-}
