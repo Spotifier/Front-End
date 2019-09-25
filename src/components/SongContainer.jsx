bafa350d869c4a9c9a68da99a5796c9be6084d6a
John Shoff <w.m.t.b5115@gmail.com>
2019-09-25 04:39:44 -0400

    Updated some styling and made the application more functionally viable.

--- src/components/SongContainer.jsx
+++ src/components/SongContainer.jsx
@@ -5,7 +5,8 @@ import Graph from "./GraphComponents/Graph";
 const SongContainer = ({ song }) => {
 console.log(song);
     return (
-        <div className="song-container">
+        <>
+        <div className="song-data">
             <div className="song-info">
                 <h2>Song</h2>
                 <hr />
@@ -14,13 +15,18 @@ console.log(song);
                 <p>Duration: {Math.floor((song.duration_ms / 1000)/ 60)}:{song.duration_ms % 60 < 10 && "0"}{song.duration_ms % 60} </p>
                 <button className="save-song-btn">Save Song</button>
             </div>
+
             <div className="song-recommendations">
                 <h2>Similar tracks you may enjoy:</h2>
                 <hr />
                 <p> stuff and stuff </p>
             </div>
-            <Graph song={song} />
         </div>
+
+            <div class="song-graph">
+                <Graph song={song} />
+            </div>
+        </>
 
     );
 }
